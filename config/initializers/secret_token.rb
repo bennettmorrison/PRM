62e096475f325a01e98208b03b8936a5c3e21f43
bennettmorrison <bennettmorrison@yahoo.com>
2013-09-03 22:02:51 -0400

    Initial Commit with Devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PRM::Application.config.secret_key_base = '5b0f257c8c4971a87574f2bcef7b124e78e32bae5631f15071ec4a8848e4e050543057d84de1a29d5c52fb6cecf2554d955843d45031e46add5038f5f24a2f0c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PRM::Application.config.session_store :cookie_store, key: '_PRM_session'
